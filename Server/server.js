18d66ed01363081623936f275226aa968c7248c7
Asif Abrar <Asif141100@gmail.com>
2023-11-27 21:02:29 +0600

    Middleware implemented

--- Server/server.js
+++ Server/server.js
@@ -80,7 +80,7 @@ app.delete('/api/students/:id', async (req, res) => {
     }
 })
 //Searching the Students
-app.get('/api/students/find', async (req, res) => {
+app.get('/api/students/find', logger, async (req, res) => {
     try {
         const { name } = req.query;
 
@@ -105,7 +105,10 @@ app.patch('/api/students/update/:id', async (req, res)=>{
 })
 
 //Middleware
-
+function logger(req, res, next){
+    console.log('Hello from middleware');
+    next();
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`)
